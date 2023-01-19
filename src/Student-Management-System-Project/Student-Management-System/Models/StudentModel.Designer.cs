558a67cbce501f3901526e08e338575d6ba0ad67
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-01-19 20:12:58 +0500

    19-01-2023

--- src/Student-Management-System-Project/Student-Management-System/Models/StudentModel.Designer.cs
+++ src/Student-Management-System-Project/Student-Management-System/Models/StudentModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\yashfsab\Source\Repos\sabay79\Web-Engineering-BootCamp-TechLift\DotNET\Student-Management-System-Project\Student-Management-System\Models\StudentModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Saba\source\repos\sabay79\Student-Management-System\src\Student-Management-System-Project\Student-Management-System\Models\StudentModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
