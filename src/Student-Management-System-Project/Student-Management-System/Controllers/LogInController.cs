77956153580901df11acd7b0e22ad1cdc73d9559
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-01-19 16:52:43 +0500

    Log In

---
+++ src/Student-Management-System-Project/Student-Management-System/Controllers/LogInController.cs
@@ -0,0 +1,45 @@
+﻿using Student_Management_System.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Student_Management_System.Controllers
+{
+    public class LogInController : Controller
+    {
+        Student_DBEntities db = new Student_DBEntities();
+
+        // GET: LogIn
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: LogIn
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(User model)
+        {
+            if(ModelState.IsValid) 
+            {
+                using(Student_DBEntities db = new Student_DBEntities()) 
+                {
+                    var user = db.Users.Where(x => x.Email == model.Email && x.Password == model.Password).FirstOrDefault();
+                    if (user != null)
+                    {
+                        Session["UserID"] = user.ID.ToString();
+                        Session["UserEmail"] = user.Email.ToString();
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("","Email or Password Incorrect");
+                    }
+                }
+            }
+            return View(model);
+        }
+    }
+}
